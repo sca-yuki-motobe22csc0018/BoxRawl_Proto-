802f2c50924ebd74b80d882977a2d7da31291922
kato_kenji <sdt22csc0002@edu.sca.ac.jp>
2024-06-12 19:08:30 +0900

    タイトル演出

--- Assets/kato/Scripts/Title/TitlePlayer.cs
+++ Assets/kato/Scripts/Title/TitlePlayer.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,16 @@ public class TitlePlayer : MonoBehaviour
 
     Rigidbody2D rg;
     int moveNum;
+    public GameObject jumpObj;
+    public GameObject dropObj;
     // Start is called before the first frame update
     void Start()
     {
         rg = this.gameObject.GetComponent<Rigidbody2D>();
         moveNum = 0;
+
+        jumpObj.SetActive(false);
+        dropObj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,6 +29,7 @@ public class TitlePlayer : MonoBehaviour
         {
             rg.velocity = new Vector2(0, 8.0f) * 1;
             isJump = true;
+            jumpObj.SetActive(true);
             moveNum = Random.Range(0, 2);
 
             if (moveNum == 1)
@@ -35,9 +42,20 @@ public class TitlePlayer : MonoBehaviour
     IEnumerator playerAttack()
     {
         yield return new WaitForSeconds(0.4f);
+        //this.gameObject.transform.DORotate(new Vector3(0, 0, 0), 1.0f, RotateMode.FastBeyond360);
         rg.gravityScale = 20.0f;
+        dropObj.SetActive(true);
 
         yield return new WaitForSeconds(0.5f);
         rg.gravityScale = 1.0f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            jumpObj.SetActive(false);
+            dropObj.SetActive(false);
+        }
     }
 }
