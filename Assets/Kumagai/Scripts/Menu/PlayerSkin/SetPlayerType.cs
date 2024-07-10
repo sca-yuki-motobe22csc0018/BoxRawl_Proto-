27494c413b11a9e88b16870f1bbed622548f7c78
kumagai <sdt22csc0005@edu.sca.ac.jp>
2024-07-10 18:35:25 +0900

    ステージ選択の選択アイコンを変更

--- Assets/Kumagai/Scripts/Menu/PlayerSkin/SetPlayerType.cs
+++ Assets/Kumagai/Scripts/Menu/PlayerSkin/SetPlayerType.cs
@@ -9,11 +9,46 @@ public class SetPlayerType : MonoBehaviour
     SpriteRenderer spriteRenderer;
     [SerializeField] Sprite[] skin;
     [SerializeField] GameObject playerSkin;
-   
+
+
+
+    private void Awake()
+    {
+        spriteRenderer = playerSkin.GetComponent<SpriteRenderer>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-       spriteRenderer=playerSkin.GetComponent<SpriteRenderer>();    
+      
+        Debug.Log(StatusUp.nowPlayerType);
+        switch (StatusUp.nowPlayerType)
+        {
+            case "Defult":
+                {
+                    spriteRenderer.sprite = skin[0];
+                    Debug.Log("a");
+                }
+                break;
+            case "Hp":
+                {
+                    Debug.Log("Hp‚Å‚·");
+                    spriteRenderer.sprite = skin[1];
+                    Debug.Log("b");
+                }
+                break;
+            case "Speed":
+                {
+                    spriteRenderer.sprite = skin[2];
+                    Debug.Log("c");
+                }
+                break;
+            case "Jump":
+                {
+                    spriteRenderer.sprite = skin[3];
+                    Debug.Log("d");
+                }
+                break;
+        }
     }
 
     // Update is called once per frame
--- Assets/Kumagai/Scripts/StageScore.cs
+++ Assets/Kumagai/Scripts/StageScore.cs
@@ -24,8 +24,6 @@ public class StageScore : MonoBehaviour
     void Update()
     {
         CheckTotalScore();
-        Debug.Log(ScoreManager.bigEnemyKillCount * 200 + ScoreManager.smallEnemyKillCount * 100
-            + (int)ScoreManager.timer * 10 + ScoreManager.lvUpCount * 2000);
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             for(int i = 0; i < scores.Length;i++)
