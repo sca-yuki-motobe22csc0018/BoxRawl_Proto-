4ec73febf398b8d9cab0f99eb146057e97d11868
AlUEO <sdt22csc0005@edu.sca.ac.jp>
2024-08-19 07:53:07 +0900

    プレイヤーアクション玉飛ばし疑似作成

---
+++ Assets/Kumagai/Scripts/JumpShot.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpShot : MonoBehaviour
+{
+    public static int shotLv;
+    private int loop;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shotDelay;
+    private bool coolTime;
+    [SerializeField]private float setCoolTimer;
+    private float coolTimer;
+    private string[] direction = { "R","L","D","U","RU","LU","RD","LD"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotLv=3;
+        coolTime=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shotLv<3)
+        {
+            loop=shotLv*2;
+        }
+        else
+        {
+            loop=8;
+        }
+        if(PlayerMove.JumpCount==0)
+        {
+            Debug.Log(loop);
+            if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+            {
+                if(!coolTime)
+                {
+                    coolTime=true;
+                    coolTimer = setCoolTimer;
+                    StartCoroutine(shot());
+                }
+            }
+        }
+        if(coolTimer>0&&coolTime)
+        {
+            Debug.Log("A");
+            coolTimer-=Time.deltaTime;
+        }
+        else if(coolTimer<0)
+        {
+            coolTime=false;
+        }
+    }
+
+    IEnumerator shot()
+    {
+        yield return new WaitForSeconds(shotDelay);
+        GameObject insObj;
+        var BM = new BulletManager();
+        for (int i = 0; i < loop; i++)
+        {
+            insObj = Instantiate(bullet, transform.position, Quaternion.identity);
+            BM = insObj.GetComponent<BulletManager>();
+            BM.moveDirection = direction[i];
+        }
+    }
+}
