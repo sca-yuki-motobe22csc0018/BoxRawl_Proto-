02c426c9da8e2e29a60174c9fa743488a502d1ba
kumagai <sdt22csc0005@edu.sca.ac.jp>
2024-08-28 17:17:31 +0900

    アクション強化　マウス対応途中

--- Assets/Kumagai/Scripts/Mouse/MouseLevelSelectAction.cs
+++ Assets/Kumagai/Scripts/Mouse/MouseLevelSelectAction.cs
@@ -23,21 +23,24 @@ public class MouseLevelSelectAction : MouseSelect
     void Update()
     {
         mousePos = Input.mousePosition;
-        Debug.Log(mousePos);
         if(mousePos.x>=200&&mousePos.x<600)
         {
-            //1
+            LevelSelect.posNum=0;
+            Debug.Log(1);
         }
 
         if(mousePos.x>=750&&mousePos.x<1200)
         {
-            //2
+            LevelSelect.posNum = 1;
+            Debug.Log(2);
         }
 
         if(mousePos.x>=1300&&mousePos.x<1700)
         {
-            //3
+            LevelSelect.posNum = 2;
+            Debug.Log(3);
         }
+        Debug.Log(LevelUpSelect.set + "SET");
     }
 
     override public void  PointerEnter()
